b47d4c9a5fd5d5bf5b6cf35e99898b0096256d14
Lafaiet Castro e Silva <lafaietsilva@gmail.com>
2022-03-26 13:47:07 +0200

    adding pages permissions

--- hollymovies/viewer/views.py
+++ hollymovies/viewer/views.py
@@ -2,7 +2,7 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from viewer.models import Movie
 from django.urls import reverse_lazy
 
@@ -32,36 +32,40 @@ class WelcomeView(TemplateView):
     template_name = 'welcome.html'
 
 
-class MoviesListView(LoginRequiredMixin, ListView):
+class MoviesListView(ListView):
     template_name = 'movies.html'
     model = Movie
     context_object_name = 'movies'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(PermissionRequiredMixin, DetailView):
     template_name = 'movie_detail.html'
     model = Movie
     context_object_name = 'movie'
+    permission_required = 'viewer.view_movie'
 
 
-class CreateMovieView(CreateView):
+class CreateMovieView(PermissionRequiredMixin, CreateView):
     template_name = 'create_movie.html'
     model = Movie
     fields = '__all__'
     success_url = reverse_lazy('movies')
+    permission_required = 'viewer.add_movie'
 
 
-class UpdateMovie(UpdateView):
+class UpdateMovie(PermissionRequiredMixin, UpdateView):
     template_name = 'update_movie.html'
     model = Movie
     success_url = reverse_lazy('movies')
     context_object_name = 'movie'
     fields = '__all__'
+    permission_required = 'viewer.change_movie'
 
 
-class DeleteMovie(DeleteView):
+class DeleteMovie(PermissionRequiredMixin, DeleteView):
     template_name = 'delete_movie.html'
     model = Movie
     context_object_name = 'movie'
     success_url = reverse_lazy('movies')
+    permission_required = 'viewer.delete_movie'
 
